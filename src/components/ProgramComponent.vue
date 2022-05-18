a5f59564532e45ac4c6f7fe14918437fd4ee4ac0
Ilthepriest <robertodelprete1993@libero.it>
2022-05-18 15:08:53 +0200

    add functionality

--- src/components/ProgramComponent.vue
+++ src/components/ProgramComponent.vue
@@ -13,7 +13,7 @@
                   <i class="italic">View Full Program</i>
               </div>
           </div>
-          <div class="row row-cols-6">
+          <!-- <div class="row row-cols-6">
               <div class="col square_day active">
                   <div class="ms-3">
                         <p class="mb-0">DAY 1</p>
@@ -46,8 +46,8 @@
               </div>
               <div class="col square_day">
               </div>
-          </div>
-          <div class="row row.cols-2 bg_color color_text p-5">
+          </div> -->
+          <!-- <div class="row row.cols-2 bg_color color_text p-5">
               <div class="col">
                   <div class="d-flex flex-column orariDestinazioni">
                       <div class="d-flex">
@@ -69,22 +69,299 @@
                   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At debitis totam quas similique culpa cumque, eos, sunt suscipit itaque, quod ad. Doloremque quam placeat saepe enim minus cupiditate nostrum distinctio, in voluptate provident odio voluptates. Sapiente, tempore sunt?</p>
                   <img src="@/assets/img/speaker-8-150x150.jpg" alt="">
               </div>
-          </div>
+          </div> -->
+            <div class="row row-cols-6">
+                <div class="col square_day" v-for="(program, index) in programs" :key="index" @click="clickDay(index)">
+                    <p class="ms-3">{{program.day}}</p>
+                    <div class="ms-3">{{program.data}}</div>
+                </div>
+                <div class="rectangle_info bg_color color_text p-4 mb-1"  v-for="(program, index) in programs[active].content" :key="index">
+                    <div class="row row-cols-2">
+                        <div class="col-2 orariDestinazioni">
+                            <div class="d-flex">
+                                <font-awesome-icon icon="fa-solid fa-clock" class="bg_icon"  v-if="program.ore"/>
+                                <p class="ms-3">{{program.ore}}</p>
+                            </div>
+                            <div class="d-flex">
+                                <font-awesome-icon icon="fa-solid fa-location-arrow" class="bg_icon"  v-if="program.destinazione"/>
+                                <p class="ms-3">{{program.destinazione}}</p>
+                            </div>
+                            <div class="d-flex">
+                                <font-awesome-icon icon="fa-solid fa-user" class="bg_icon"  v-if="program.speaker"/>
+                                <div class="d-flex flex-column">
+                                    <p class="ms-3" id="color_froly">{{program.speaker}}</p>
+                                    <p class="ms-3" id="color_froly">{{program.speaker2}}</p>
+                                    <p class="ms-3" id="color_froly">{{program.speaker3}}</p>
+                                </div>
+                            </div>
+                            </div>
+                            
+                        <div class="col-10 introduction">
+                            <h5>{{program.title}}</h5>
+                            <p>{{program.text}}</p>
+                            <div id="color_froly" class="bold">{{program.oraPranzo}}</div>
+                            <h2 class="bold">{{program.descrizione}}</h2>
+                            <img :src="program.image" alt="">
+                            <img :src="program.image2" alt="">
+                            <img :src="program.image3" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
       </div>
   </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            active: 0,
+        programs:[
+            {
+                day: "DAY 1",
+                data: "23 May 2016",
+                content: [
+                    {
+                        ore: "09:00 - 10:30",
+                        destinazione: "ROOM A",
+                        speaker: "LAURENCE FRANCIS",
+                        title: "WELCOMING AND INTRODUCTION",
+                        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur natus incidunt officiis ad nemo molestiae fuga quia corrupti dolores consequatur",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-8-150x150.jpg",
+                        id: 213,
+                    }
+                ]
+            },
 
+            {
+                day: "DAY 2",
+                data: "23 May 2016",
+                content: [
+                    {
+                        ore: "10:30 - 10:00",
+                        destinazione: "ROOM ALEXANDER X",    
+                        speaker:"PATRICK SPENCER",
+                        speaker2: "JONATHAN ELVES",
+                        title: "GATHERING DEMAND INFORMATION",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-6-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-1-150x150.jpg",
+                        id: 2342,
+                    },
+                    {
+                        oraPranzo: "12:00 - 13:00",
+                        descrizione: "INTERMISSION & LUNCH BREAK"
+                    },
+                    {
+                        ore: "13:00 - 14:30",
+                        destinazione: "ROOM C",
+                        speaker: "JANET JONES",
+                        speaker2: "ANGELINA HOLY",
+                        title: "SETTING UP YOUR BUSINESS PLAN",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-5-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-3-150x150.jpg",
+                        id: 2342,
+                    },
+                    {
+                        ore: "14:30 - 16:00",
+                        destinazione: "ROOM C",
+                        speaker: "MATTHEW WHITE",
+                        title: "FINDING SUPPLIER",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-2-150x150.jpg",
+                        id: 2342,
+                    }
+                ]
+            },
+            
+            {
+                day: "DAY 3",
+                data: "24 May 2016",
+                content: [
+                    {
+                        ore: "9:00 - 10:30",
+                        destinazione: "ROOM A",
+                        speaker: "JANET JONES",  
+                        speaker2: "MICHAEL DOVER",
+                        speaker3: "ANGELINA HOLY",
+                        title: "DEALING WITH SUPPLIER",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-5-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-4-150x150.jpg",
+                        image3: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-3-150x150.jpg",
+                        id: 2672,
+                    },
+                    {
+                        ore: "9:00 - 10:30",
+                        destinazione: "ROOM D",
+                        speaker: "JOHN SUPER DOE", 
+                        speaker2: "PATRICK SPENCER",
+                        title: "MARKETING THEORY",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-7-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-6-150x150.jpg",
+                        id: 2672, 
+                    },
+                    {
+                        ore: "10:30 - 12:00",
+                        destinazione: "ROOM b",
+                        speaker: "JOHN SUPER DOE",
+                        speaker2: "JONATHAN ELVES",
+                        title: "WHAT TO DO BEFORE START CREATING PRODUCT",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-7-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-1-150x150.jpg",
+                        id: 2672,
+                    },
+                    {
+                        oraPranzo: "12:00 - 13:00",
+                        descrizione: "INTERMISSION & LUNCH BREAK"
+                    },
+                    {
+                        ore: "13:00 - 14:30",
+                        destinazione: "ROOM C",
+                        speaker: "LAURENT FRANCIS", 
+                        speaker2: "ANGELINA HOLY",
+                        title: "CREATING PRODUCTIVITY 1",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-8-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-3-150x150.jpg",
+                        id: 2672,
+                    },
+                    {
+                        ore: "14:30 - 16:00",
+                        destinazione: "ROOM C",
+                        speaker: "ANGELINA HOLY",
+                        speaker2: "JONATHAN ELVES",
+                        title: "CREATING PRODUCTIVITY 2",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-3-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-1-150x150.jpg",
+                        id: 2672,
+                    }
+                ]
+            },
+            {
+                day: "DAY 4",
+                data: "25 May 2030",
+                content: [
+                    {
+                        ore: "10:00 - 12:00",
+                        destinazione: "ROOM A",
+                        speaker: "PATRICK SPENCER", 
+                        speaker2: "MICHAEL DOVER",
+                        title: "PRODUCT LINES",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-6-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-4-150x150.jpg",
+                        id: 2672,
+                    },
+                    {
+                        oraPranzo: "12:00 - 13:00",
+                        descrizione: "INTERMISSION & LUNCH BREAK"
+                    },
+                    {
+                        ore: "13:00 - 14:30",
+                        destinazione: "ROOM D",
+                        speaker: "ANGELINA HOLY",
+                        title: "QUALITY CONTROL",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-3-150x150.jpg",
+                        id: 2672, 
+                    },
+                    {
+                        ore: "14:30 - 16:00",
+                        destinazione: "ROOM D",
+                        speaker: "JOHN SUPER DOE",
+                        title: "FURNISHING PROCESS",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-7-150x150.jpg",
+                        id: 2672,
+                    }
+                ]
+            },
+            {
+                day: "DAY 5",
+                data: "26 May 2030",
+                content: [
+                    {
+                        ore: "09:00 - 10:30",
+                        destinazione: "ROOM B",
+                        speaker: "LAURENCE FRANCIS", 
+                        speaker2: "JANET JONES",
+                        title: "PRODUCT PRESENTATION",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-8-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-5-150x150.jpg",
+                        id: 2672,
+                    },
+                    {
+                        ore: "10:30 - 12:00",
+                        destinazione: "ROOM C",
+                        speaker: "MICHAEL DOVER",
+                        title: "MARKETING YOUR PRODUCT",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-4-150x150.jpg",
+                        id: 2672, 
+                    },
+                    {
+                        oraPranzo: "12:00 - 13:00",
+                        descrizione: "INTERMISSION & LUNCH BREAK"
+                    },
+                    {
+                        ore: "13:00 - 14:30",
+                        destinazione: "ROOM C",
+                        speaker: "JOHN SUPER DOE",
+                        speaker2: "PATRICK SPENCER",
+                        title: "SOCIAL MEDIA CAN HELP",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-7-150x150.jpg",
+                        image2: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-6-150x150.jpg",
+                        id: 2672,
+                    },
+                    {
+                        ore: "14:30 - 16:00",
+                        destinazione: "ROOM A",
+                        speaker: "MATTHEW WHITE",
+                        title: "ENDING SESSION",
+                        text: "Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur....",
+                        image: "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-2-150x150.jpg",
+                        id: 2672,
+                    }
+                ]
+            },
+            {
+
+            }
+        ],
+        }
+    }, 
+     methods:{
+        clickDay(i){
+            this.active = i
+            console.log("mi hai clickato");
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.bold{
+    font-weight: 600;
+    font-size: 26px;
+}
+
+.rectangle_info{
+    width: 100%;
+    height: 250px;
+}
 .square_day{
     padding: 10px;
     background-color: orangered;
     border-right: 1px solid $muted;
+    cursor: pointer;
    p{
         color: white;
         font-size: 16px;
@@ -125,7 +402,7 @@ export default {
 }
 .bg_icon{
     color: $gray;
-    font-size: 20px;
+    font-size: 15px;
 }
 h3{
     font-size: 36px;
--- src/components/SpeakersComponent.vue
+++ src/components/SpeakersComponent.vue
@@ -1,22 +1,41 @@
 <template>
-<div class="bg-container">
-  <div class="container bg-container py-5">
+  <div class="bg-container">
+    <div class="container bg-container py-5">
       <div class="row row-cols-2 py-5 d-flex">
-          <div class="col">
-             <h3>SPEAKERS</h3> 
+        <div class="col">
+          <h3>SPEAKERS</h3>
+        </div>
+        <div class="col">
+          <div class="arrow d-flex justify-content-end">
+            <div
+              class="
+                square_arrow
+                p-4
+                d-flex
+                justify-content-center
+                align-items-center
+                me-2
+              "
+              @click="scroll_left"
+            >
+              <font-awesome-icon icon="fa-solid fa-angle-left" />
+            </div>
+            <div
+              class="
+                square_arrow
+                p-4
+                d-flex
+                justify-content-center
+                align-items-center
+              "
+              @click="scroll_right"
+            >
+              <font-awesome-icon icon="fa-solid fa-angle-right" />
+            </div>
           </div>
-          <div class="col">
-              <div class="arrow d-flex justify-content-end">
-                  <div class="square_arrow p-4 d-flex justify-content-center align-items-center me-2">
-                      <font-awesome-icon icon="fa-solid fa-angle-left" />
-                  </div>
-                  <div class="square_arrow p-4 d-flex justify-content-center align-items-center">
-                    <font-awesome-icon icon="fa-solid fa-angle-right" />
-                  </div>
-              </div>
-          </div>
+        </div>
       </div>
-      <div class="row row-cols-4 py-5 text-center">
+      <!-- <div class="row row-cols-4 py-5 text-center">
           <div class="col">
                <div class="orange position-relative">
                   <div class="icone">
@@ -73,72 +92,167 @@
                     <p>Maxii's Manager</p>
               </div>
           </div>
+      </div> -->
+      <div class="row row-cols-4 d-flex flex-nowrap overflow wrapper">
+        <div
+          class="col"
+          v-for="(speaker, index) in speakers[active].utente"
+          :key="index"
+        >
+          <div class="orange position-relative">
+            <div class="icone">
+              <font-awesome-icon class="icon" icon="fa-brands fa-twitter" />
+              <font-awesome-icon class="icon" icon="fa-brands fa-facebook-f" />
+              <font-awesome-icon class="icon" icon="fa-brands fa-linkedin-in" />
+            </div>
+            <img :src="speaker.photo" alt="" />
+          </div>
+          <div class="speaker mt-4">
+            <div>{{ speaker.nome }}</div>
+            <p>{{ speaker.mansione }}</p>
+          </div>
+        </div>
       </div>
-
+    </div>
   </div>
-</div>  
 </template>
 
 <script>
 export default {
-    name: 'SiteSpeakers'
-}
+  name: "SiteSpeakers",
+  data() {
+    return {
+      active: 0,
+      speakers: [
+        {
+          utente: [
+            {
+              nome: "LAURENCE FRANCIS",
+              mansione: "Google's UI Designer",
+              photo:
+                "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-8.jpg",
+            },
+            {
+              nome: "JOHN SUPER DOE",
+              mansione: "Yahoo's HR",
+              photo:
+                "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-7.jpg",
+            },
+            {
+              nome: "PATRICK SPENCER",
+              mansione: "S&P Analyzer",
+              photo:
+                "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-6.jpg",
+            },
+            {
+              nome: "JANET JONES",
+              mansione: "Newyork Post's GM",
+              photo:
+                "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-5.jpg",
+            },
+            {
+              nome: "MICHAEL DOVER",
+              mansione: "Starbuck's CEO",
+              photo:
+                "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-4.jpg",
+            },
+            {
+              nome: "JANET JONES",
+              mansione: "Newyork Post's GM",
+              photo:
+                "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-3.jpg",
+            },
+            {
+              nome: "MATTHEW WHITE",
+              mansione: "Facebook's Co-Founder",
+              photo:
+                "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-2.jpg",
+            },
+            {
+              nome: "JONATHAN ELVES",
+              mansione: "Twitter's Co-Founder",
+              photo:
+                "https://demo.goodlayers.com/thekeynote/wp-content/uploads/2014/12/speaker-1.jpg",
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    scroll_left() {
+      let content = document.querySelector(".wrapper");
+      content.scrollLeft -= 330;
+    },
+    scroll_right() {
+      let content = document.querySelector(".wrapper");
+      content.scrollLeft += 330;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.icone .icon{
-    margin-left: 7px;
-    margin-right: 7px;
+.overflow {
+  overflow-x: overlay;
 }
-.icone{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 40px;
-    color: white;
-    visibility: hidden;
+::-webkit-scrollbar {
+  background: transparent;
+}
+.icone .icon {
+  margin-left: 7px;
+  margin-right: 7px;
+}
+.icone {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 40px;
+  color: white;
+  visibility: hidden;
 }
 
-.orange{
-    background-color: rgba(242, 56, 26);
-    width: 300px;
-    border-radius: 10px;
+.orange {
+  background-color: rgba(242, 56, 26);
+  width: 300px;
+  border-radius: 10px;
 }
-[class*= "orange"]:hover .icone{
-    visibility: visible;
+[class*="orange"]:hover .icone {
+  visibility: visible;
 }
 img:hover {
-    opacity: 0.2;
+  opacity: 0.2;
 }
 
-.bg-container{
-    background-color: #F3F3F3;
+.bg-container {
+  background-color: #f3f3f3;
 }
-.square_arrow{
-    width: 30px;
-    height: 30px;
-    background-color: $scarlet;
-    color: white;
-    border-radius: 5px;
+.square_arrow {
+  width: 30px;
+  height: 30px;
+  background-color: $scarlet;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
 }
-h3{
+h3 {
+  font-weight: 600;
+  font-size: 36px;
+}
+img {
+  width: 300px;
+  border-radius: 10px;
+}
+.speaker {
+  div {
     font-weight: 600;
-    font-size: 36px;
-}
-img{
-    width: 300px;
-    border-radius: 10px ;
-}
-.speaker{
-    div{
-        font-weight: 600;
-        font-size: 15px;
-    }
-    p{
-        color: $gray;
-        font-size: 13px;
-        margin-top: 10px;
-    }
+    font-size: 15px;
+  }
+  p {
+    color: $gray;
+    font-size: 13px;
+    margin-top: 10px;
+  }
 }
 </style>
